0e9407f932c71d169bc6fd9d1a6795edb5b52983
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-05-05 15:38:29 -0700

    pushing photo

--- poems/main_app/views.py
+++ poems/main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.db import models
-from .models import Poem
+from .models import Poem, Photo
 from .forms import CommentForm
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -9,7 +9,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.models import User
+import uuid
+import boto3
 
+S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
+BUCKET = 'poe-app'
 # Create your views here.
 # from django.http import HttpResponse
 
@@ -51,16 +55,6 @@ def poems_detail(request, poem_id):
   comment_form = CommentForm()
   return render(request, 'poems/detail.html', { 'poem': poem, 'comment_form': comment_form })
 
-# @login_required
-# def add_comment(request, poem_id):
-#   form = CommentForm(request.POST)
-#   if form.is_valid():
-#     form.instance.user = request.user
-#     new_comment = form.save(commit=False)
-#     new_comment.poem_id = poem_id
-#     new_comment.save() 
-#   # return redirect(poem_id=poem_id)
-#   return redirect('/poems', poem_id=poem_id)
 @login_required
 def add_comment(request, poem_id):
   form = CommentForm(request.POST)
@@ -91,3 +85,22 @@ class PoeDelete(LoginRequiredMixin, DeleteView):
 
 def genres(request):
   return render(request, 'genres.html')  
+
+def add_photo(request, poem_id):
+    # photo-file will be the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            # we can assign to cat_id or cat (if you have a cat object)
+            photo = Photo(url=url, poem_id=poem_id)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('detail', poem_id=poem_id)
